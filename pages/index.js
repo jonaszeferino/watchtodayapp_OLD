a125184ae1963c98cab96ac1b28e1a5042c2c286
jonaszeferino <75401113+jonaszeferino@users.noreply.github.com>
2023-02-17 01:00:13 -0300

    home images and CSS

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Link from "next/link";
+import Image from "next/image";
 import Head from "next/head";
+import styles from "../styles/Home.module.css";
 export default function Home() {
   return (
     <div>
@@ -8,27 +10,101 @@ export default function Home() {
         <meta name="keywords" content="nba,franchise,teams"></meta>
         <meta name="description" content="encontre tudo de nba aqui"></meta>
       </Head>
-      <h1>Ferramentas de Busca</h1>
+
       <br />
-      <Link href="/searchmovies">
-        <a>Descubra os Filmes</a>
-      </Link>
-      <br />
-      <Link href="/watchtoday">
-        <a>Que filme assitir hoje?</a>
-      </Link>
-      <br />
-      <Link href="/searchmoviesbycountry">
-        <a>Filmes Por Língua</a>
-      </Link>
-      <br />
-      <Link href="/searchtvshows">
-        <a>Busca de Séries</a>
-      </Link>
-      <br />
-      <Link href="/searchfree">
-        <a>Busca Livre</a>
-      </Link>
+
+      <div className={styles.container}>
+        <div>
+          <h2> Descubra Filmes</h2>
+          <div className={styles.card_image_home}>
+            <Link href="/searchmovies">
+              <a>
+                {" "}
+                <Image
+                  src="/DescubraFilmes.png"
+                  alt="poster"
+                  width="360"
+                  height="600"
+                />
+              </a>
+            </Link>
+          </div>
+        </div>
+
+        <br />
+
+        <div>
+          <h2> O que Assitir Hoje?</h2>
+          <div className={styles.card_image_home}>
+            <Link href="/watchtoday">
+              <a>
+                <Image
+                  src="/OqueAssistirHojeFix.png"
+                  alt="poster"
+                  width="360"
+                  height="600"
+                />
+              </a>
+            </Link>
+          </div>
+        </div>
+        <br />
+
+        <div>
+          <h2> Filmes Por Língua</h2>
+          <div className={styles.card_image_home}>
+            <Link href="/searchmoviesbycountry">
+              <a>
+                <Image
+                  src="/CinemaCountries.png"
+                  alt="poster"
+                  width="360"
+                  height="600"
+                />
+              </a>
+            </Link>
+          </div>
+        </div>
+        <br />
+
+        <div>
+          <h2> Descubra Séries</h2>
+          <div className={styles.card_image_home}>
+            <Link href="/searchtvshows">
+              <a>
+                {" "}
+                <Image
+                  src="/tvShows.png"
+                  alt="poster"
+                  width="360"
+                  height="600"
+                />
+              </a>
+            </Link>
+          </div>
+        </div>
+        <br />
+
+        <div>
+          <h2>Busca Livre</h2>
+          <div className={styles.card_image_home}>
+            <Link href="/searchfree">
+              <a>
+                {" "}
+                <Image
+                  src="/freeSearch.png"
+                  alt="poster"
+                  width="360"
+                  height="600"
+                />
+              </a>
+            </Link>
+          </div>
+        </div>
+        <br />
+
+        <br />
+      </div>
     </div>
   );
 }
