a4e48831d8d74b1beeae9e7aa19978a60c764d68
jonaszeferino <jonaszeferino@gmail.com>
2023-08-07 23:54:33 -0300

    new page season details with new responsive css

--- pages/tvshow-seasons-page.js
+++ pages/tvshow-seasons-page.js
@@ -1,17 +1,20 @@
 import { useState, useEffect } from "react";
 import Link from "next/link";
 import styles from "../styles/Home.module.css";
-import Image from "next/image";
 import { format } from "date-fns";
 import { useRouter } from "next/router";
 import {
   ChakraProvider,
   Progress,
-  Table,
-  Tbody,
   Tr,
   Td,
+  Box,
+  Center,
+  Text,
+  Table,
+  Tbody,
   TableContainer,
+  Image,
 } from "@chakra-ui/react";
 
 const MoviePage = () => {
@@ -62,56 +65,89 @@ const MoviePage = () => {
   return (
     <>
       <ChakraProvider>
-        <div className={styles.grid}>
-          {dataTvShows.episodes ? (
-            dataTvShows.episodes.map((episode) => (
-              <div key={episode.name} className={styles.gridItem}>
-                <span className={styles.title}>{episode.name}</span>
-                <br />
-                <br />
-                <img
-                  // className={styles.card_image}
-                  src={
-                    episode.still_path
-                      ? "https://image.tmdb.org/t/p/original" +
-                        episode.still_path
-                      : "/callback.png"
-                  }
-                  alt="poster"
-                  width="1280"
-                  height="720"
-                />
-                <div>
-                  <TableContainer>
-                    <Table size="sm">
-                      <Tbody>
-                        <Tr>
-                          <Td>Título em Português:</Td>
-                          <Td>{episode.name}</Td>
-                        </Tr>
+        <Center minHeight="100vh">
+          <Box maxW="576px" width="100%" px="4">
+            <div className={styles.grid}>
+              {dataTvShows.episodes ? (
+                dataTvShows.episodes.map((episode) => (
+                  <div
+                    key={episode.name}
+                    className={styles.gridItem}
+                    style={{
+                      maxWidth: "100%", // Use relative unit
+                      margin: "0 auto",
+                      wordBreak: "break-word",
+                    }}
+                  >
+                    <Box my="6" textAlign="center">
+                      <Text fontSize="xl" fontWeight="semibold">
+                        {episode.name} E{episode.episode_number}
+                      </Text>
+                      <Box
+                        width="100%"
+                        mx="auto"
+                        my="4"
+                        boxShadow="3px 8px 12px rgba(0.4, 0.4, 0.4, 0.4)"
+                      >
+                        <Image
+                          src={
+                            episode.still_path
+                              ? "https://image.tmdb.org/t/p/original" +
+                                episode.still_path
+                              : "/callback.png"
+                          }
+                          alt="poster"
+                          maxW="100%"
+                          height="auto"
+                        />
+                      </Box>
 
-                        <Tr>
-                          <Td>Overview:</Td>
-                          <Td>
-                            {episode.overview ? episode.overview : "Sem infos"}
-                          </Td>
-                        </Tr>
-
-                        <Tr>
-                          <Td>Nota Média:</Td>
-                          <Td>{episode.vote_average}</Td>
-                        </Tr>
-                      </Tbody>
-                    </Table>
-                  </TableContainer>
-                </div>
-                <br />
-              </div>
-            ))
-          ) : (
-            <p>Nenhum episódio encontrado.</p>
-          )}
-        </div>
+                      <div
+                        style={{
+                          maxWidth: "100%",
+                          margin: "0 auto",
+                          wordBreak: "break-word",
+                        }}
+                      >
+                        <TableContainer>
+                          <Table size="sm">
+                            <Tbody>
+                              <Tr>
+                                <Td>Título em Português:</Td>
+                                <Td>{episode.name}</Td>
+                              </Tr>
+                              <Tr>
+                                <Td>Overview:</Td>
+                                <Td
+                                  style={{
+                                    wordWrap: "break-word",
+                                    maxWidth: "100%", // Use relative unit
+                                  }}
+                                >
+                                  {episode.overview
+                                    ? episode.overview
+                                    : "Sem infos"}
+                                </Td>
+                              </Tr>
+                              <Tr>
+                                <Td>Nota Média:</Td>
+                                <Td>{episode.vote_average}</Td>
+                              </Tr>
+                            </Tbody>
+                          </Table>
+                        </TableContainer>
+                      </div>
+                    </Box>
+                  </div>
+                ))
+              ) : (
+                <Text fontSize="xl" fontWeight="semibold" textAlign="center">
+                  Nenhum episódio encontrado.
+                </Text>
+              )}
+            </div>
+          </Box>
+        </Center>
       </ChakraProvider>
     </>
   );
